da320b33ad52849e1c72a61858aca038132656bf
AmelBoe <156824843+AmelBoe@users.noreply.github.com>
2024-02-07 17:04:55 +0100

    Hover Button: Wirf einen Blick in die Oper

--- VersuchderBoxenmitdemBallerinamarker/script.js
+++ VersuchderBoxenmitdemBallerinamarker/script.js
@@ -2,41 +2,41 @@
 
 
 //Button zum 360 Gradbild
- // document.querySelector('#button2').addEventListener('click', function() {
-   // document.querySelector('#operBild').setAttribute('visible', true);
-    //document.querySelector('#page4').setAttribute('visible', true);
-  //});
+// document.querySelector('#button2').addEventListener('click', function() {
+// document.querySelector('#operBild').setAttribute('visible', true);
+//document.querySelector('#page4').setAttribute('visible', true);
+//});
 
-  document.getElementById("websiteButton").addEventListener("click", function () {
-    console.log("hallo Welt")
-    document.getElementById("fullscreenImage").object3D.visible = true;
-    document.querySelector('#page1').setAttribute('visible', false);
-    document.querySelector('#nextButton').setAttribute('visible', false);
-    document.querySelector('#nextButton2').setAttribute('visible', false);
+document.getElementById("websiteButton").addEventListener("click", function () {
+  console.log("hallo Welt")
+  document.getElementById("fullscreenImage").object3D.visible = true;
+  document.querySelector('#page1').setAttribute('visible', false);
+  document.querySelector('#nextButton').setAttribute('visible', false);
+  document.querySelector('#nextButton2').setAttribute('visible', false);
 }
 )
 
 document.getElementById("buttonOper").addEventListener("click", function () {
   console.log("Oper Bild")
- // document.getElementById("fullscreenImage").setAttribute("visible","false");
+  // document.getElementById("fullscreenImage").setAttribute("visible","false");
   document.getElementById("operBild").object3D.visible = true;
-  
+
   //document.getElementById("feldSzene").object3D.visible = true;
 }
 )
 
 
 //Wenn man auf "Wo" klickt, wird man zu Google Maps weitergeleitet
-document.querySelector('#textWo').addEventListener('click', function() {
+document.querySelector('#textWo').addEventListener('click', function () {
   window.open("https://www.google.com/maps/place/Wiener+Staatsoper/@48.2034341,16.3666285,17z/data=!3m2!4b1!5s0x476d079c632e16c7:0x369a4e45f874ca56!4m6!3m5!1s0x476d079c861dbfd5:0xfd59268d25377d56!8m2!3d48.2034306!4d16.3692034!16zL20vMDIxYzh2?entry=ttu", "_blank")
 });
 
 
 //Wenn man über Wo hovert, ändert sich die Schriftfarbe
-document.querySelector("#textWo").addEventListener("mouseenter",function(){
+document.querySelector("#textWo").addEventListener("mouseenter", function () {
   document.querySelector("#textWo").setAttribute("color", "grey")
 });
-document.querySelector("#textWo").addEventListener("mouseleave",function(){
+document.querySelector("#textWo").addEventListener("mouseleave", function () {
   document.querySelector("#textWo").setAttribute("color", "#FFF")
 });
 
@@ -45,16 +45,29 @@ document.querySelector("#textWo").addEventListener("mouseleave",function(){
 let websiteButton = document.querySelector('#websiteButton');
 
 
-websiteButton.addEventListener('mouseenter', function() {
-    websiteButton.setAttribute('position', '0 0.5 -0.3');
- 
-});
-
-websiteButton.addEventListener('mouseleave', function() {
-    websiteButton.setAttribute('position', '0 0.5 -0.1');
+websiteButton.addEventListener('mouseenter', function () {
+  websiteButton.setAttribute('position', '0 0.5 -0.3');
 
 });
 
+websiteButton.addEventListener('mouseleave', function () {
+  websiteButton.setAttribute('position', '0 0.5 -0.1');
+
+});
+
+//wenn man über den Button "Wirf einen Blick in die Oper" hovert, dann kommt er leicht entgegen
+let buttonOper = document.querySelector('#buttonOper');
+
+
+buttonOper.addEventListener('mouseenter', function () {
+  buttonOper.setAttribute('position', '0 0.5 -0.3');
+
+});
+
+buttonOper.addEventListener('mouseleave', function () {
+  buttonOper.setAttribute('position', '0 0.5 -0.1');
+
+});
 
 
 
@@ -68,12 +81,12 @@ let ballerinabild1 = document.createElement("a-image");
 let count = 0;
 
 function bildwechsel() {
-    // let hintergrund1 = document.createElement("a-image"); 
-    ballerinabild1.setAttribute("src", ballerinabild[count]);
-    count++;  //Counter wird bei jedem Aufrufen der Funktion um 1 erhöht 
-    if (count == ballerinabild.length) {
-        count = 0
-    }
+  // let hintergrund1 = document.createElement("a-image"); 
+  ballerinabild1.setAttribute("src", ballerinabild[count]);
+  count++;  //Counter wird bei jedem Aufrufen der Funktion um 1 erhöht 
+  if (count == ballerinabild.length) {
+    count = 0
+  }
 
 }
 ballerinabild1.setAttribute("scale", "4 4 4"); //Veränderung der Größe der Ballerina
@@ -82,8 +95,8 @@ ballerinabild1.setAttribute("position", "0 0 -5")
 //Intervall der Bilder Ballerina
 intervall = setInterval(() => {
 
-    console.log("intervall Ballerina Bilder");
-    bildwechsel();
+  console.log("intervall Ballerina Bilder");
+  bildwechsel();
 
 }, 25);
 
@@ -93,8 +106,8 @@ document.querySelector("#marker").appendChild(ballerinabild1);
 
 //Sound wird erst abgespielt, wenn der Marker gescannt wird
 document.querySelector('#marker').addEventListener('markerFound', function () {
-    document.querySelector('#sound').setAttribute('visible', true);
-    document.querySelector('#sound').components.sound.playSound();
+  document.querySelector('#sound').setAttribute('visible', true);
+  document.querySelector('#sound').components.sound.playSound();
 });
 
 
@@ -102,16 +115,16 @@ document.querySelector('#marker').addEventListener('markerFound', function () {
 //Klick durch die Seiten (Karussell)
 document.querySelector("#nextButton").addEventListener("click", function () {
   if (document.getElementById('page1').object3D.visible) {
-      document.getElementById('page1').object3D.visible = false;
-      document.getElementById('page2').object3D.visible = true;
+    document.getElementById('page1').object3D.visible = false;
+    document.getElementById('page2').object3D.visible = true;
   } else if (document.getElementById('page2').object3D.visible) {
-      document.getElementById('page2').object3D.visible = false;
-      document.getElementById('page3').object3D.visible = true;
+    document.getElementById('page2').object3D.visible = false;
+    document.getElementById('page3').object3D.visible = true;
   } else if (document.getElementById('page3').object3D.visible) {
-      document.getElementById('page3').object3D.visible = false;
-      document.getElementById('page4').object3D.visible = true;
+    document.getElementById('page3').object3D.visible = false;
+    document.getElementById('page4').object3D.visible = true;
   } else if (document.getElementById('page4').object3D.visible) {
-      document.getElementById('page4').object3D.visible = false;
-      document.getElementById('page1').object3D.visible = true;
+    document.getElementById('page4').object3D.visible = false;
+    document.getElementById('page1').object3D.visible = true;
   }
 });
